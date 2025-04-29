a0d4f70a25364718940c63dcf3899faae8fb22a7
AbbottSze <94067281+AbbottSze@users.noreply.github.com>
2025-04-29 21:06:57 +0100

    Robot position CBC position

--- CBC.js
+++ CBC.js
@@ -8,7 +8,7 @@ let plaintextInput = document.getElementById("CBCInput");
 let ivInput = document.getElementById("IV");
 
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight * 0.75;
+canvas.height = window.innerHeight * 0.72;
 
 // CBC encryption variables + initial values
 let blockSize = 50;
